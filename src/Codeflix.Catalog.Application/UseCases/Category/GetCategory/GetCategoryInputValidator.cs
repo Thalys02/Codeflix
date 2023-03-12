5793bcad04c638f86d321fea7e399e330ba64883
Thalys Melicio <thalys.melicio02@gmail.com>
2023-03-12 17:39:36 -0300

    feat: added GetCategoryInputValidator

---
+++ src/Codeflix.Catalog.Application/UseCases/Category/GetCategory/GetCategoryInputValidator.cs
@@ -0,0 +1,12 @@
+﻿using FluentValidation;
+
+namespace Codeflix.Catalog.Application.UseCases.Category.GetCategory
+{
+    public class GetCategoryInputValidator : AbstractValidator<GetCategoryInput>
+    {
+        public GetCategoryInputValidator()
+        {
+            RuleFor(x => x.Id).NotEmpty();
+        }
+    }
+}
---
+++ tests/Codeflix.Catalog.UnitTests/Application/GetCategory/GetCategoryInputValidatorTest.cs
@@ -0,0 +1,46 @@
+﻿using Codeflix.Catalog.Application.UseCases.Category.GetCategory;
+using FluentAssertions;
+using Xunit;
+
+namespace Codeflix.Catalog.UnitTests.Application.GetCategory
+{
+    [Collection(nameof(GetCategoryTestFixture))]
+    public class GetCategoryInputValidatorTest
+    {
+        private readonly GetCategoryTestFixture _fixture;
+
+        public GetCategoryInputValidatorTest(GetCategoryTestFixture fixture)
+        {
+            _fixture = fixture;
+        }
+        [Fact(DisplayName = nameof(ValidationOk))]
+        [Trait("Application", "GetCategoryInputValidator - UseCases")]
+        public void ValidationOk()
+        {
+            var validInput = new GetCategoryInput(Guid.NewGuid());
+            var validator = new GetCategoryInputValidator();
+
+            var validResult = validator.Validate(validInput);
+
+            validResult.Should().NotBeNull();
+            validResult.IsValid.Should().BeTrue();
+            validResult.Errors.Should().HaveCount(0);
+        }
+
+        [Fact(DisplayName = nameof(InvalidWhenEmptyGuidId))]
+        [Trait("Application", "GetCategoryInputValidator - UseCases")]
+        public void InvalidWhenEmptyGuidId()
+        {
+            var invalidInput = new GetCategoryInput(Guid.Empty);
+            var validator = new GetCategoryInputValidator();
+
+            var validResult = validator.Validate(invalidInput);
+
+            validResult.Should().NotBeNull();
+            validResult.IsValid.Should().BeFalse();
+            validResult.Errors.Should().HaveCount(1);
+            validResult.Errors[0].ErrorMessage.Should().Be("'Id' must not be empty.");
+
+        }
+    }
+}
